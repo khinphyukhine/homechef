d1f82a1184b71156e9104449e0bd71d2456f898f
Khin Phyu Khine <kkhine3@gatech.edu>
2021-06-07 18:38:54 -0400

    Minor Changes

--- homechef/src/App.js
+++ homechef/src/App.js
@@ -5,7 +5,7 @@ import {v4 as uuidv4} from 'uuid';
 import Recipe from "./ components/Recipe"
 import Alert from "./ components/Alert";
 import NavBar from "./ components/navbar/NavBar"
-//import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 export default function App() {
     const [query, setQuery] = useState("");
